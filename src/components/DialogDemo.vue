21d846383cdc94a35ceb0a13ba3ed4d2bf9464d8
twodays <1064575122@qq.com>
2021-03-01 16:32:08 +0800

    添加 dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,17 +1,36 @@
 <template>
-    <div>
-       Dialog
-    </div>
+        <h2>Dialog 组件</h2>
+        <h1>示例1</h1>
+        <Button @click="toggle">toggle</Button>
+        <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+            <template v-slot:content>
+                <div>猪猪子是高级忍者</div>
+            </template>
+            <template v-slot:title>
+                <strong>Title</strong>
+            </template>
+        </Dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, h } from 'vue';
+import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
 
 export default defineComponent({
+    components: {Dialog, Button},
     setup () {
-        
-
-        return {}
+        const x = ref(false);
+        const toggle = ()=>{
+           x.value = !x.value;
+           console.log(x);
+           console.log(x.value);
+        }
+        const f1 = () => {
+            return false
+        }
+        const f2 = () => {}
+        return {x, toggle, f1, f2}
     }
 })
 </script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,132 @@
+<template>
+{{visible}}
+    <div v-if="visible">
+       <div class="guoguo-dialog-overlay" @click="onClickOverlay"></div>
+            <div class="guoguo-dialog-wrapper">
+                <div class="guoguo-dialog">
+                    <header>
+                        <slot name="title" />
+                        <span @click="close" class="guoguo-dialog-close"></span>
+                    </header>
+                    <main>
+                        <slot name="content" />
+                    </main>
+                    <footer>
+                        <Button level="main" @click="ok">OK</Button>
+                        <Button @click="cancel">Cancel</Button>
+                    </footer>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from './Button.vue';
+
+export default defineComponent({
+    components: {Button},
+    props: {
+        visible: {
+            type: Boolean,
+            default: false,
+        },
+        closeOnClickOverlay: {
+            type: Boolean,
+            default: true,
+        },
+        ok: {
+            type:Function
+        }
+    },
+    setup (props, context) {
+        const close = () => {
+            context.emit('update:visible', false)
+        }
+        const onClickOverlay = () => {
+            console.log(1)
+            if(props.closeOnClickOverlay) {
+                close();
+            }
+        }
+        const ok = () => {
+            if (props.ok() === false) {
+                close()
+            }
+        }
+        const cancel = () => {
+            close()
+        }
+        return {close, onClickOverlay, ok, cancel}
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+$radius: 4px;
+$border-color: #d9d9d9;
+
+.guoguo-dialog {
+    background: #fff;
+    // padding: 16px;
+    min-width: 15em;
+    max-width: 90%;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade-out($color: #000000, $amount: 0.5);
+    &-overlay {
+        position:fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: fade-out($color: #000000, $amount: 0.5);
+        z-index: 10;
+    }
+    &-wrapper {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 11;
+    }
+    >header {
+        padding: 12px 16px;
+        border-bottom: 1px solid $border-color;
+        display: flex;
+        justify-content: space-between;
+        align-items:center;
+        font-size: 20px;
+    }
+    >main {
+        padding: 1.5em 16px;
+    }
+    >footer {
+        padding: 12px 16px;
+        border-top: 1px solid $border-color;
+        text-align: right;
+    }
+    &-close {            
+        position: relative;
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        cursor: pointer;
+        &::before, &::after {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 1px;
+            background: #000;
+            top: 50%;
+            left: 50%;
+        }
+        &::before {
+            transform: translate(-50%, -50%) rotate(-45deg);
+        }
+        &::after {
+            transform: translate(-50%, -50%) rotate(45deg);
+        }
+    }
+   
+}
+</style>
